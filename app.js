b3afd00beeee45a0032ad44391fc400bcce2d6f1
HakemShubar <AbduelhakemGAbdusalam.Shubar@experian.com>
2019-05-28 04:34:06 +0800

    adding result and report feature

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 
 mongoose.connect('mongodb://localhost/medishift');
 let db = mongoose.connection;
@@ -84,33 +85,91 @@ app.post('/input_data', function (request, response) {
 
     let medicalRecords = new MedicalRecords();
     if (dataType.includes('Temperature')) {
-        medicalRecords.temperature = (data.length > 1) ? data[dataType.indexOf('Temperature')] : data;
+        medicalRecords.temperature = (Array.isArray(data)) ? data[dataType.indexOf('Temperature')] : data;
     }
     if (dataType.includes('Blood Pressure')) {
-        let bloodPressure = (data.length > 1) ? data[dataType.indexOf('Blood Pressure')] : data;
+        let bloodPressure = (Array.isArray(data)) ? data[dataType.indexOf('Blood Pressure')] : data;
         bloodPressure = bloodPressure.split('/');
         medicalRecords.systolic = bloodPressure[0];
         medicalRecords.diastolic = bloodPressure[1];
     }
     if (dataType.includes('Heartbeats Rate')) {
-        medicalRecords.heartbeats_rate = (data.length > 1) ? data[dataType.indexOf('Heartbeats Rate')] : data;
+        medicalRecords.heartbeats_rate = (Array.isArray(data)) ? data[dataType.indexOf('Heartbeats Rate')] : data;
     }
     if (dataType.indexOf('Oxygen Level') > -1) {
-        medicalRecords.oxygen_level = (data.length > 1) ? data[dataType.indexOf('Oxygen Level')] : data;
+        medicalRecords.oxygen_level = (Array.isArray(data)) ? data[dataType.indexOf('Oxygen Level')] : data;
     }
     if (dataType.includes('Weight')) {
 
-        medicalRecords.weight = (data.length > 1) ? data[dataType.indexOf('Weight')] : data;
+        medicalRecords.weight = (Array.isArray(data)) ? data[dataType.indexOf('Weight')] : data;
     }
+    console.log(new Date(Date.parse("2005-07-08T11:22:33+0000")));
     medicalRecords.created_date = new Date();
     console.log(medicalRecords);
-    medicalRecords.save(function (error) {
+    // medicalRecords.save(function (error) {
+    //     if (error) {
+    //         console.log(error);
+    //     }
+        // response.render('result_views/result', {
+        //     value: medicalRecords
+        // })
+        response.render('result_views/result', {
+            isSaved: 1,
+            medicalRecords: medicalRecords
+        });
+    // });
+});
+
+app.get('/report', function (request, response) {
+    MedicalRecords.find({}, function (error, medicalRecords) {
         if (error) {
             console.log(error);
+            return;
         }
-        response.redirect('/');
+        response.render('report_views/report', {
+            medicalRecords: medicalRecords
+        });
     });
 });
+
+app.get('/report/download', function (request, response) {
+    MedicalRecords.find({}, function (error, medicalRecords) {
+        if (error) {
+            console.log(error);
+            return;
+        }
+
+        const csvWriter = createCsvWriter({
+            path: 'medicalRecords.csv',
+            header: [
+                {id: 'temperature', title: 'Temperature'},
+                {id: 'systolic', title: 'Blood Pressure (systolic)'},
+                {id: 'diastolic', title: 'Blood Pressure (diastolic)'},
+                {id: 'heartbeats_rate', title: 'Heartbeats Rate'},
+                {id: 'oxygen_level', title: 'Blood Oxygen Level'},
+                {id: 'weight', title: 'Weight'},
+                {id: 'created_date', title: 'Input Date'},
+            ]
+        });
+
+        csvWriter
+            .writeRecords(medicalRecords)
+            .then(()=> console.log('The CSV file was written successfully'));
+
+        response.redirect('/report');
+
+        // fs.createReadStream('data.csv')
+        //     .pipe(csv())
+        //     .on('data', (medicalRecords) => {
+        //         console.log(medicalRecords);
+        //     })
+        //     .on('end', () => {
+        //         console.log('CSV file successfully processed');
+        //     });
+    });
+});
+
+
 
 app.listen(8000, function () {
     console.log('server started in port 8000')
--- public/js/nav_bar.js
+++ public/js/nav_bar.js
@@ -14,19 +14,3 @@ $(".tabs").on("click", "a", function (e) {
     // localStorage.setItem("selectedTab", this.getAttribute("id"));
     window.location.href = this.getAttribute("href");
 });
-
-
-// on load of the page: switch to the currently selected tab
-$(document).ready(function () {
-    var tab = document.getElementById(localStorage.getItem("selectedTab"));
-    if (tab == null) {
-        tab = document.getElementById("your_health");
-    }
-    $(tab).addClass('active');
-    var activeWidth = $(tab).innerWidth();
-    var itemPos = $(tab).position();
-    $(".selector").css({
-        "left": itemPos.left + "px",
-        "width": activeWidth + "px"
-    });
-});
